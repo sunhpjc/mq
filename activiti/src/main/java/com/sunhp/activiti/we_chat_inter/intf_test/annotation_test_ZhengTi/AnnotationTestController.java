cff040afbb38b30110510e10454496a3a4615cac
sunhpjc <2274278354@qq.com>
2022-11-08 22:06:22 +0800

    logback日志xml基本修改完成

--- activiti/src/main/java/com/sunhp/activiti/we_chat_inter/intf_test/annotation_test_ZhengTi/AnnotationTestController.java
+++ activiti/src/main/java/com/sunhp/activiti/we_chat_inter/intf_test/annotation_test_ZhengTi/AnnotationTestController.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sunhp.activiti.dao.UserDao;
 import com.sunhp.activiti.exception.MyException;
 import com.sunhp.activiti.we_chat_inter.intf_test.annotation_test_ZhengTi.config.ChannelEnums;
 import com.sunhp.activiti.we_chat_inter.intf_test.annotation_test_ZhengTi.config.StrategyFactory;
@@ -24,6 +26,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("smsTestAnnotation")
 @Slf4j
 public class AnnotationTestController {
+    private final UserDao userDao;
+
+    public AnnotationTestController(UserDao userDao) {this.userDao = userDao;}
 
     @GetMapping("/testAnnotation")
     public void testAnnotation(String channelCode) {
@@ -67,6 +72,18 @@ public class AnnotationTestController {
                 log.info("渠道异常 渠道编号：{} 异常：{}",str, e);
             }
         }
+    }
+
+    @GetMapping("/testDebug")
+    public void testDebug(){
+        log.info("测试info日志");
+        log.debug("测试debug日志");
+        log.error("测试error日志");
+    }
+
+    @GetMapping("testSqlLog")
+    public void getUser(int id){
+        userDao.queryById(id);
     }
 
 }
